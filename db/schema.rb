23bdda9d02c84725f712d2f303793cbdd44b1d3e
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-03 23:34:27 -0400

    Update schema.rb to add user_id column and foreign key constraint in links table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_27_233800) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_04_032726) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_27_233800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "links", "users"
 end
