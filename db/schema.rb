0dc8eb281c36bece768cfc51101997404c4992db
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-11 23:45:39 -0400

    Update schema.rb to add votes table with foreign key constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_04_032726) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_12_034433) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
@@ -28,5 +28,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_04_032726) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
