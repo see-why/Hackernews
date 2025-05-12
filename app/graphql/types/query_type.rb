8fe2f8fdca7694a9fb0a1ff48404af20a870f779
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-05-12 01:37:26 -0400

    Replace all_links field implementation with LinksSearch resolver for improved query handling

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,9 +21,6 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :all_links, [ LinkType ], null: false
-    def all_links
-      Link.all
-    end
+    field :all_links, resolver: Resolvers::LinksSearch
   end
 end
